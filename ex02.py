b0283d6f0cd9580741c02b571c212927c4ef8e4d
HidekiLeonardo <leonardohideki778@gmail.com>
2024-09-01 18:15:59 -0300

    Resolvendo problemas

--- ex02.py
+++ ex02.py
@@ -1,9 +1,13 @@
 def is_fibonacci(num):
     a, b = 0, 1
+    if num < 0:
+        return False
     while b < num:
         a, b = b, a + b
-        
+    return b == num
+
 num = int(input("Digite um número: "))
+
 if is_fibonacci(num):
     print(f"O número {num} pertence à sequência de Fibonacci.")
 else:
